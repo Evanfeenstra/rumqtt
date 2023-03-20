afe5de2b29ba0d9a6b8d90234fb785160a05b815
Evan Feenstra <evanfeenstra@gmail.com>
2023-03-19 23:26:00 -0700

    ConsoleSettings::new()

--- rumqttd/src/lib.rs
+++ rumqttd/src/lib.rs
@@ -141,6 +141,12 @@ pub struct ConsoleSettings {
 }
 
 impl ConsoleSettings {
+    pub fn new(listen: &str) -> Self {
+        Self {
+            listen: listen.to_string(),
+            filter_handle: None,
+        }
+    }
     pub fn set_filter_reload_handle(&mut self, handle: ReloadHandle) {
         self.filter_handle.replace(handle);
     }
